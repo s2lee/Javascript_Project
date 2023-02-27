4710c8cb18de397926c2fcf9f5060c272ec8f7ff
s2lee <therok.manager@gmail.com>
2023-02-27 19:55:50 +0900

    Add js_engine callstack

---
+++ js_engine/callstack.js
@@ -0,0 +1,32 @@
+const x = 'x1';
+function c() {
+    const y = 'y';
+    console.log('c');
+    debugger;
+    function e() {
+        const k = 'k';
+        console.log('e');
+        c();
+    }
+}
+
+function a() {
+    const x = 'x2';
+    console.log('a');
+    function b() {
+        const z = z;
+        console.log('b');
+        c();
+    }
+    b();
+    console.log(x);
+}
+
+function d() {
+    console.log('d');
+    // e(); error
+}
+a(); // a, b, c
+c(); // c
+
+// 스코프체인 - 함수에서 어떤 값에 접근이 가능한가
