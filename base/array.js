69a4db766ab26cb776dabfff73f3e8c907ac0c66
s2lee <therok.manager@gmail.com>
2023-03-06 20:17:47 +0900

    array

--- base/array.js
+++ base/array.js
@@ -48,3 +48,50 @@ while (index > -1) {
     target.splice(index, 1);
     index = target.indexOf('라');
 }
+
+let days = ['월', '화', '수'];
+for (let index = 0; index < days.length; index++) {
+    console.log(days[index]);
+}
+
+// 객체를 순회하는 for ~ in 과 혼동 X -> 배열 순회는 for ~ of 사용
+for (let day of days) {
+    console.log(day);
+}
+
+/*
+1. concat : 합쳐서 새배열 반환
+2. forEach(fn) : 함수를 인자로 받아서 배열 반복
+3. filter(fn) : 만족하는 모든 요소를 배열로 반환
+4. map(fn) : 함수를 받아 특정 기능을 시행하고 새로운 배열을 반환
+*/
+
+const arr2 = [1, 2];
+console.log(arr2.concat([3, 4])); // [1, 2, 3, 4]
+
+const arr3 = ['Mike', 'Tom', 'Jane'];
+arr3.forEach((name, index) => {
+    console.log(`${index + 1}. ${name}`);
+});
+
+const arr4 = [1, 2, 3, 4, 5, 6];
+const result3 = arr4.filter((item) => {
+    return item % 2 == 0;
+});
+console.log(result3);
+
+const userList = [
+    { name: 'Mike', age: 30 },
+    { name: 'Jane', age: 27 },
+    { name: 'Tom', age: 10 },
+];
+
+const newUserList = userList.map((user, index) => {
+    return Object.assign({}, user, {
+        id: index + 1,
+        isAdult: user.age > 19,
+    });
+});
+
+console.log(newUserList);
+console.log(userList);
