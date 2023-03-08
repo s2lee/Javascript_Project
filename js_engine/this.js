8f2a923fdf94ea9f719354976170a4bd599f92d5
s2lee <therok.manager@gmail.com>
2023-03-08 21:12:23 +0900

    this

--- js_engine/this.js
+++ js_engine/this.js
@@ -1,5 +1,12 @@
-// this 는 기본적으로 window
-// this 는 함수 호출 때 결정
+/*
+1. this 는 함수 호출 때 결정
+2. this 는 기본적으로 window
+3. 화살표 함수는 일반 함수와는 달리 자신만의 this를 가지지 않음
+4. 화살표 함수는 부모의 this를 그대로 가져옴
+5. 객체의 메서드를 작성할 때는 화살표 함수로 작성하지 않는게 좋다
+6. this 를 사용해 객체에 접글한다면 화살표 함수 사용 X
+
+*/
 
 const obj = {
     name: 'seungseok',
@@ -55,3 +62,33 @@ sayN(); // window.name undefined
 obj.sayName2(); // window.name undefined
 
 obj.sayName3();
+
+const header = document.querySelector('.class');
+header.addEventListener('click', function () {
+    console.log(this); // this == header
+});
+
+let boy = {
+    name: 'Mike',
+    sayHello: () => {
+        console.log(this); // 전역객체 - 브라우저 환경: window, Node js : global
+    },
+};
+
+boy.sayHello();
+
+let girl = {
+    name: 'Jane',
+    showName: function () {
+        // console.log(girl.name); // 메서드에서는 객체명을 직접 써주기 보다는 this 사용하기
+        console.log(this.name);
+    },
+    sayBye() {
+        console.log(`Good bye ${this.name}`);
+    },
+};
+let woman = girl;
+woman.showName();
+girl = null;
+// woman.showName(); 에러
+woman.sayBye();
